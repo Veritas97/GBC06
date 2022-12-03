ef4ab9409f5d01f26c7ee4cf145587da4763793f
Veritas97 <lykovcl@gmail.com>
2022-12-03 15:39:50 +0900

    Answer task42

---
+++ Task42/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+string NumBinary(int numA)
+{
+    string numB = string.Empty;
+    while (numA > 0)
+    {
+        numB = numA % 2 + numB;
+        numA /= 2;
+    }
+
+    return numB;
+}
+
+string BinaryNumber = NumBinary(numberN);
+Console.WriteLine($"Ваше двоичное число: {BinaryNumber}");

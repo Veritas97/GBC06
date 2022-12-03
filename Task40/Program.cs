f27a6531df1e751ec97e980920692ca882794613
Veritas97 <lykovcl@gmail.com>
2022-12-03 15:09:12 +0900

    Answer task40

---
+++ Task40/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число первое: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число второе: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число третье: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+if (numberA + numberB > numberC && 
+    numberA + numberC > numberB &&
+    numberB + numberC > numberA) 
+    Console.WriteLine("Такой треугольник может существовать!");
+else Console.WriteLine("Такой треугольник не может существовать!");

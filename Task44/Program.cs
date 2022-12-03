466d35f36b5e8fc343d41526223b12c30c6da10a
Veritas97 <lykovcl@gmail.com>
2022-12-03 16:38:28 +0900

    Answer task44

---
+++ Task44/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int[] FibonachiNumber(int numb)
+{
+    int[] arr = new int[numb];
+    arr[1] = 1;
+
+    for (int i = 2; i < numb; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+
+    Console.WriteLine("]");
+}
+
+int[] fib = FibonachiNumber(numberN);
+PrintArray(fib);
